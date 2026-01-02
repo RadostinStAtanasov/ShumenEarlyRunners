504cd57db73ead2e4eb894ff75d9470cdef8556a
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-01-02 11:24:32 +0200

    creating gallery ForUs

--- frontend/src/assets/pages/ForUs.jsx
+++ frontend/src/assets/pages/ForUs.jsx
@@ -1,19 +1,520 @@
-import homeImage from "../images/home-page.bmp";
-import classes from "./Home.module.css";
+import classes from "./ForUs.module.css";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+// import first from "../images/1.jpg";
+// import second from "../images/2.jpg";
+// import third from "../images/3.jpg";
+// import forth from "../images/4.jpg";
+// import five from "../images/5.jpg";
+// import six from "../images/6.jpg";
+// import seven from "../images/7.jpg";
+// import eight from "../images/8.jpg";
+// import nine from "../images/9.jpg";
+// import ten from "../images/10.jpg";
+// import eleven from "../images/11.jpg";
+import first2 from "../images/picsFromRunning/1.jpg";
+import second2 from "../images/picsFromRunning/2.jpg";
+import third2 from "../images/picsFromRunning/3.jpg";
+import forth2 from "../images/picsFromRunning/4.jpg";
+import five2 from "../images/picsFromRunning/5.jpg";
+import six2 from "../images/picsFromRunning/6.jpg";
+import seven2 from "../images/picsFromRunning/7.jpg";
+import eight2 from "../images/picsFromRunning/8.jpg";
+import nine2 from "../images/picsFromRunning/9.jpg";
+import ten2 from "../images/picsFromRunning/10.jpg";
+import first3 from "../images/picsFromRunning/11.jpg";
+import second3 from "../images/picsFromRunning/12.jpg";
+import third3 from "../images/picsFromRunning/13.jpg";
+import forth3 from "../images/picsFromRunning/14.jpg";
+import five3 from "../images/picsFromRunning/15.jpg";
+import six3 from "../images/picsFromRunning/16.jpg";
+import seven3 from "../images/picsFromRunning/17.jpg";
+import eight3 from "../images/picsFromRunning/18.jpg";
+import nine3 from "../images/picsFromRunning/19.jpg";
+import ten3 from "../images/picsFromRunning/20.jpg";
+import first4 from "../images/picsFromRunning/21.jpg";
+import second4 from "../images/picsFromRunning/22.jpg";
+import third4 from "../images/picsFromRunning/23.jpg";
+import forth4 from "../images/picsFromRunning/24.jpg";
+import five4 from "../images/picsFromRunning/25.jpg";
+import six4 from "../images/picsFromRunning/26.jpg";
+import seven4 from "../images/picsFromRunning/27.jpg";
+import eight4 from "../images/picsFromRunning/28.jpg";
+import nine4 from "../images/picsFromRunning/29.jpg";
+import ten4 from "../images/picsFromRunning/30.jpg";
+import first5 from "../images/picsFromRunning/31.jpg";
+import second5 from "../images/picsFromRunning/32.jpg";
+import third5 from "../images/picsFromRunning/33.jpg";
+import forth5 from "../images/picsFromRunning/34.jpg";
+import five5 from "../images/picsFromRunning/35.jpg";
+import six5 from "../images/picsFromRunning/36.jpg";
+import seven5 from "../images/picsFromRunning/37.jpg";
+import eight5 from "../images/picsFromRunning/38.jpg";
+import nine5 from "../images/picsFromRunning/39.jpg";
+import ten5 from "../images/picsFromRunning/40.jpg";
+import first6 from "../images/picsFromRunning/41.jpg";
+import second6 from "../images/picsFromRunning/42.jpg";
+import third6 from "../images/picsFromRunning/43.jpg";
+import forth6 from "../images/picsFromRunning/44.jpg";
+import five6 from "../images/picsFromRunning/45.jpg";
+import six6 from "../images/picsFromRunning/46.jpg";
+import seven6 from "../images/picsFromRunning/47.jpg";
+import eight6 from "../images/picsFromRunning/48.jpg";
+import nine6 from "../images/picsFromRunning/49.jpg";
+import ten6 from "../images/picsFromRunning/50.jpg";
+import first7 from "../images/picsFromRunning/51.jpg";
+import second7 from "../images/picsFromRunning/52.jpg";
+import third7 from "../images/picsFromRunning/53.jpg";
+import forth7 from "../images/picsFromRunning/54.jpg";
+import five7 from "../images/picsFromRunning/55.jpg";
+import six7 from "../images/picsFromRunning/56.jpg";
+import seven7 from "../images/picsFromRunning/57.jpg";
+import eight7 from "../images/picsFromRunning/58.jpg";
+import nine7 from "../images/picsFromRunning/59.jpg";
+import ten7 from "../images/picsFromRunning/60.jpg";
+import first8 from "../images/picsFromRunning/61.jpg";
+import second8 from "../images/picsFromRunning/62.jpg";
+import third8 from "../images/picsFromRunning/63.jpg";
+import forth8 from "../images/picsFromRunning/64.jpg";
+import five8 from "../images/picsFromRunning/65.jpg";
+import six8 from "../images/picsFromRunning/66.jpg";
+import seven8 from "../images/picsFromRunning/67.jpg";
+import eight8 from "../images/picsFromRunning/68.jpg";
+import nine8 from "../images/picsFromRunning/69.jpg";
+import ten8 from "../images/picsFromRunning/70.jpg";
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
 
 export default function ForUsPage() {
   return (
-    <div>
-      За насс страницат?!otbor smeshnici
-      <div className={classes.runEatSleep}>
-        <img
-          src={homeImage}
-          alt="home page image"
-          width={1000}
-          height={800}
-          style={{ alignItems: "center" }}
-        />
-      </div>
+    <div className={classes.mainContainer}>
+      <ImageList
+        sx={{ width: 2000, height: 950 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
+        {itemData.map((item) => (
+          <ImageListItem
+            key={item.img}
+            cols={item.cols || 1}
+            rows={item.rows || 1}
+          >
+            <img
+              className={classes.im}
+              {...srcset(item.img, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
+
+const itemData = [
+  // {
+  //   img: first,
+  //   title: "Breakfast",
+  //   rows: 2,
+  //   cols: 2,
+  // },
+  // {
+  //   img: second,
+  //   title: "Burger",
+  // },
+  // {
+  //   img: third,
+  //   title: "Camera",
+  // },
+  // {
+  //   img: forth,
+  //   title: "Coffee",
+  //   cols: 2,
+  // },
+  // {
+  //   img: five,
+  //   title: "Hats",
+  //   cols: 2,
+  // },
+  // {
+  //   img: six,
+  //   title: "Honey",
+  //   author: "@arwinneil",
+  //   rows: 2,
+  //   cols: 2,
+  // },
+  // {
+  //   img: seven,
+  //   title: "Basketball",
+  // },
+  // {
+  //   img: eight,
+  //   title: "Fern",
+  // },
+  // {
+  //   img: nine,
+  //   title: "Mushrooms",
+  //   rows: 2,
+  //   cols: 2,
+  // },
+  // {
+  //   img: ten,
+  //   title: "Tomato basil",
+  // },
+  // {
+  //   img: eleven,
+  //   title: "Sea star",
+  // },
+  {
+    img: first2,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second2,
+    title: "Burger",
+  },
+  {
+    img: third2,
+    title: "Camera",
+  },
+  {
+    img: forth2,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five2,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six2,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven2,
+    title: "Basketball",
+  },
+  {
+    img: eight2,
+    title: "Fern",
+  },
+  {
+    img: nine2,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten2,
+    title: "Tomato basil",
+  },
+  {
+    img: first3,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second3,
+    title: "Burger",
+  },
+  {
+    img: third3,
+    title: "Camera",
+  },
+  {
+    img: forth3,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five3,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six3,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven3,
+    title: "Basketball",
+  },
+  {
+    img: eight3,
+    title: "Fern",
+  },
+  {
+    img: nine3,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten3,
+    title: "Tomato basil",
+  },
+  {
+    img: first4,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second4,
+    title: "Burger",
+  },
+  {
+    img: third4,
+    title: "Camera",
+  },
+  {
+    img: forth4,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five4,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six4,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven4,
+    title: "Basketball",
+  },
+  {
+    img: eight4,
+    title: "Fern",
+  },
+  {
+    img: nine4,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten4,
+    title: "Tomato basil",
+  },
+  {
+    img: first5,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second5,
+    title: "Burger",
+  },
+  {
+    img: third5,
+    title: "Camera",
+  },
+  {
+    img: forth5,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five5,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six5,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven5,
+    title: "Basketball",
+  },
+  {
+    img: eight5,
+    title: "Fern",
+  },
+  {
+    img: nine5,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten5,
+    title: "Tomato basil",
+  },
+  {
+    img: first6,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second6,
+    title: "Burger",
+  },
+  {
+    img: third6,
+    title: "Camera",
+  },
+  {
+    img: forth6,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five6,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six6,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven6,
+    title: "Basketball",
+  },
+  {
+    img: eight6,
+    title: "Fern",
+  },
+  {
+    img: nine6,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten6,
+    title: "Tomato basil",
+  },
+  {
+    img: first7,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second7,
+    title: "Burger",
+  },
+  {
+    img: third7,
+    title: "Camera",
+  },
+  {
+    img: forth7,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five7,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six7,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven7,
+    title: "Basketball",
+  },
+  {
+    img: eight7,
+    title: "Fern",
+  },
+  {
+    img: nine7,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten7,
+    title: "Tomato basil",
+  },
+  {
+    img: first8,
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: second8,
+    title: "Burger",
+  },
+  {
+    img: third8,
+    title: "Camera",
+  },
+  {
+    img: forth8,
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: five8,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: six8,
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: seven8,
+    title: "Basketball",
+  },
+  {
+    img: eight8,
+    title: "Fern",
+  },
+  {
+    img: nine8,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: ten8,
+    title: "Tomato basil",
+  },
+];
--- frontend/src/assets/pages/Home.jsx
+++ frontend/src/assets/pages/Home.jsx
@@ -1,12 +1,18 @@
 import fiveKmImage from "../images/giveKmshNew.bmp";
 import classes from "../pages/Home.module.css";
+import Button from "@mui/material/Button";
 
 export default function HomePage() {
   return (
     <>
-      <div className={classes.firstVontainer}>
-        <div className={classes.image}>
-          <img className={classes.images} src={fiveKmImage} alt="" />
+      <div>
+        <div className={classes.firstVontainer}>
+          <div className={classes.image}>
+            <img className={classes.images} src={fiveKmImage} alt="" />
+            <Button variant="contained" className={classes.buttonFiveKm}>
+              Виж къде бягаме всяка събота Кликкк..!!!
+            </Button>
+          </div>
         </div>
       </div>
     </>

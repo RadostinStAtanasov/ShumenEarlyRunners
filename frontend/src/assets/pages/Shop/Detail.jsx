303632e8fb13df7ffd230c859f1d31926dfa6f49
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-01-19 08:09:04 +0200

    detail page 70% ready

--- frontend/src/assets/pages/Shop/Detail.jsx
+++ frontend/src/assets/pages/Shop/Detail.jsx
@@ -5,46 +5,61 @@ import tShirt3 from "../../images/tshirtImages/tshirt3.jpg";
 
 export default function DetailPage() {
   return (
-    <div className={classes.mainContainer}>
+    <>
       <h1>
         За да поръчаш се обади :0878688970 или изпрати имейл: irinova@yahoo.com
       </h1>
-      <div className={classes.imageDetails}>
-        <img src={tShirt1} alt="" />
-      </div>
-      <div className={classes.containerDescription}>
-        <h2>Тениска Early Runners</h2>
-        <h3>в наличност</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          totam accusantium pariatur. Optio aliquam quidem pariatur ut ex porro
-          libero ullam commodi fugit, numquam similique atque aperiam ratione
-          quaerat fugiat magnam mollitia tempora! Nisi porro dolor possimus ad
-          placeat, consequuntur animi architecto eveniet deleniti ipsa a,
-          tenetur ab veritatis similique iusto debitis reprehenderit molestias
-          fugit eos, expedita perferendis non. Quaerat aliquid atque debitis
-          temporibus exercitationem porro facere reprehenderit sunt modi odio.
-          Illo a quidem veritatis temporibus saepe est, atque voluptatum placeat
-          recusandae molestiae voluptates qui consequuntur culpa magnam quaerat
-          blanditiis expedita, dolorem laudantium. Doloremque itaque
-          exercitationem explicabo magnam repudiandae fuga.
-        </p>
-        <div>26.36 € / 52лв.</div>
-        <div className={classes.containerSexSize}>
-          <div>
-            <div>мъжко/дамско</div>
-            <div>izbirane na vyzmojnost ot pada6toto menu</div>
+      <div className={classes.mainContainer}>
+        <div className={classes.imageDetails}>
+          <img className={classes.image} src={tShirt1} alt="" />
+        </div>
+        <div className={classes.containerDescription}>
+          <h2 className={classes.productTitle}>
+            Тениска за бягане | Early Runners
+          </h2>
+          <h3 className={classes.inStock}>в наличност</h3>
+          <div className={classes.justUnderline}> </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            totam accusantium pariatur. Optio aliquam quidem pariatur ut ex
+            porro libero ullam commodi fugit, numquam similique atque aperiam
+            ratione quaerat fugiat magnam mollitia tempora! Nisi porro dolor
+            possimus ad placeat, consequuntur animi architecto eveniet deleniti
+            ipsa a, tenetur ab veritatis similique iusto debitis reprehenderit
+            molestias fugit eos, expedita perferendis non. Quaerat aliquid atque
+            debitis temporibus exercitationem porro facere reprehenderit sunt
+            modi odio. Illo a quidem veritatis temporibus saepe est, atque
+            voluptatum placeat recusandae molestiae voluptates qui consequuntur
+            culpa magnam quaerat blanditiis expedita, dolorem laudantium.
+            Doloremque itaque exercitationem explicabo magnam repudiandae fuga.
+          </p>
+          <div className={classes.price}>26.36 € / 52лв.</div>
+          <div className={classes.containerSexSize}>
+            <div className={classes.sizeAndSex}>
+              <label>МЪЖКА / ДАМСКА</label>
+              <select>
+                <option value="xs">мъжка</option>
+                <option value="s">дамска</option>
+              </select>
+            </div>
+            <div className={classes.sizeAndSex}>
+              <label>РАЗМЕР</label>
+              <select>
+                <option value="xs">XS</option>
+                <option value="s">S</option>
+                <option value="m">M</option>
+                <option value="l">L</option>
+                <option value="x">X</option>
+                <option value="xl">XL</option>
+              </select>
+            </div>
           </div>
           <div>
-            <div>razmer</div>
-            <div>izbirane na vyzmojnost ot pada6toto menu</div>
+            <button>+1-</button>
+            <div>icona s koshnica i kupi nadpis</div>
           </div>
         </div>
-        <div>
-          <button>+1-</button>
-          <div>icona s koshnica i kupi nadpis</div>
-        </div>
       </div>
-    </div>
+    </>
   );
 }

514e157c836573342be9473703b7a765fa99a32f
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-01-26 07:38:12 +0200

    contact page ready

--- frontend/src/assets/pages/Contact.jsx
+++ frontend/src/assets/pages/Contact.jsx
@@ -1,31 +1,81 @@
 import classes from "../pages/Contacts.module.css";
-import forest from "../images/home-page.bmp";
+import monument from "../images/pametnik.jpg";
+import shumenskaKrepost from "../images/glavna-snimka-shumenska-krepost.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPersonThroughWindow } from "@fortawesome/free-solid-svg-icons";
+import { faThumbtack } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactPage() {
   return (
-    <div className={classes.mainContainer}>
+    <>
       <div className={classes.headerAndUnderline}>
         <h1 className={classes.title}>Контакти</h1>
         <div className={classes.underline}></div>
       </div>
-      <div className={classes.containerForm}>
-        <div className={classes.containerFormContacts}>
-          <div>На живо</div>
-          <div>По телефона</div>
-          <div>По емейла</div>
-          <div>Как да се свържеш с нас ела всяка събота на живо 08:30</div>
-          ТУК или на ТЕЛ: 1234567890/ емайл: асдфас@yahoo.цом
+      <div className={classes.mainContainer}>
+        <div className={classes.containerImagesContacts}>
+          <div className={classes.rotateRight}>
+            <img
+              className={classes.imagesContacts}
+              src={monument}
+              alt="Pulpit rock"
+            />
+
+            <p className={classes.paragraph}>
+              „Създателите на българската държава“, известен също като „1300
+              години България“, е архитектурен комплекс, посветен на
+              1300-годишнината от създаването на българската държава. Монументът
+              е разположен в най-високата част на Дунавската равнина.
+            </p>
+          </div>
+
+          <div className={classes.rotateLeft}>
+            <img
+              className={classes.imagesContacts}
+              src={shumenskaKrepost}
+              alt="Monterosso al Mare"
+            />
+
+            <p className={classes.paragraph}>
+              Крепостта се намира на два километра западно от град Шумен. В
+              района се намира важен кръстопът на маршрути, свързващи дунавския
+              бряг (при Силистра) с Адрианопол и Константинопол, и северното
+              черноморско крайбрежие със западните български земи.
+            </p>
+          </div>
         </div>
-        <div className={classes.images}>
-          <img className={classes.image} src={forest} alt="" />
-        </div>
-        <div className={classes.connectWithUsContainer}>
-          <input type="text" />
-          <textarea name="" id=""></textarea>
-          <button>Submit</button>
+        <div className={classes.containerForm}>
+          <div className={classes.containerFormContacts}>
+            <div>
+              <FontAwesomeIcon
+                className={classes.icons}
+                icon={faPersonThroughWindow}
+              />
+              На живо: Как да се свържеш с нас ела всяка събота 08:30
+              <a href="https://www.google.com/maps/place/Parking/@43.2578646,26.9224865,123m/data=!3m1!1e3!4m6!3m5!1s0x40a58aef74d63f0d:0x5a1f94674ff36d5f!8m2!3d43.2579096!4d26.9230636!16s%2Fg%2F11cjhq28bh?entry=ttu&g_ep=EgoyMDI2MDEyMS4wIKXMDSoASAFQAw%3D%3D">
+                <FontAwesomeIcon icon={faThumbtack} />
+              </a>
+            </div>
+            <div>
+              <FontAwesomeIcon className={classes.icons} icon={faPhone} />
+              По телефона: 0878688970
+            </div>
+            <div>
+              <FontAwesomeIcon className={classes.icons} icon={faEnvelope} />
+              По емейла: irinova@yahoo.com
+            </div>
+          </div>
+          <div className={classes.connectWithUsContainer}>
+            <span>Пиши ни на нашата контактна форма</span>
+            <input type="text" />
+            <textarea name="" id=""></textarea>
+            <button className={classes.btnSubmit}>Изпрати</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- frontend/src/assets/pages/Home.jsx
+++ frontend/src/assets/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function HomePage() {
         <div className={classes.firstContainer}>
           <div className={classes.image}>
             <img className={classes.images} src={fiveKmImage} alt="" />
-            <a href="https://www.google.com/maps/dir//9700+%D0%A8%D1%83%D0%BC%D0%B5%D0%BD/@43.2576384,26.9221636,284m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x40a58af18efaf5d5:0xa60b88a05b941a9b!2m2!1d26.9229305!2d43.2618806?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D">
+            <a href="https://www.google.com/maps/place/Parking/@43.2578646,26.9224865,123m/data=!3m1!1e3!4m6!3m5!1s0x40a58aef74d63f0d:0x5a1f94674ff36d5f!8m2!3d43.2579096!4d26.9230636!16s%2Fg%2F11cjhq28bh?entry=ttu&g_ep=EgoyMDI2MDEyMS4wIKXMDSoASAFQAw%3D%3D">
               <Button variant="contained" className={classes.buttonFiveKm}>
                 Виж къде бягаме всяка събота 5км от 08:30 на Шуменското плато
                 Кликкк..!!!

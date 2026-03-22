6573a50c8a425c51a567d1361aa4506a9614d217
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-03-22 09:35:22 +0200

    Update queries.js

--- backend/queries.js
+++ backend/queries.js
@@ -16,58 +16,58 @@ const getBlogs = (req, res) => {
   });
 };
 
-// const getUserById = (req, res) => {
-//   const id = parseInt(req.params.id);
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
 
-//   pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
-//     if (error) {
-//       throw error;
-//     }
-//     res.status(200).json(results.rows);
-//   });
-// };
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
 
-// const createUser = (req, res) => {
-//   const { name, email } = req.body;
+const createUser = (req, res) => {
+  const { name, email } = req.body;
 
-//   pool.query(
-//     "INSERT INTO users (name, email) VALUES ($1, $2)",
-//     [name, email],
-//     (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
-//       res.status(201).send(`User added with ID: ${results.insertId}`);
-//     },
-//   );
-// };
+  pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2)",
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`User added with ID: ${results.insertId}`);
+    },
+  );
+};
 
-// const updateUser = (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const { name, email } = req.body;
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, email } = req.body;
 
-//   pool.query(
-//     "UPDATE users SET name = $1, email = $2 WHERE id = $3",
-//     [name, email, id],
-//     (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
-//       res.status(200).send(`User modified with ID: ${id}`);
-//     },
-//   );
-// };
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`User modified with ID: ${id}`);
+    },
+  );
+};
 
-// const deleteUser = (req, res) => {
-//   const id = parseInt(req.params.id);
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
 
-//   pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
-//     if (error) {
-//       throw error;
-//     }
-//     res.status(200).send(`User delete with ID: ${id}`);
-//   });
-// };
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(`User delete with ID: ${id}`);
+  });
+};
 
 module.exports = {
   getBlogs,

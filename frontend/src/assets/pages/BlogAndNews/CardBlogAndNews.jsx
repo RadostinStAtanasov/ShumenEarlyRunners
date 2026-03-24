8c060802a6e9b0539daf69c6834c151629f3ab5f
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-03-24 07:51:19 +0200

    Update CardBlogAndNews.jsx

--- frontend/src/assets/pages/BlogAndNews/CardBlogAndNews.jsx
+++ frontend/src/assets/pages/BlogAndNews/CardBlogAndNews.jsx
@@ -2,6 +2,7 @@ import classes from "../BlogAndNews/CardBlogAndNews.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import imgs from "../../images/blogAndNewsImages/5kmRD.jpg";
 
 export default function CardBlogAndNewsPage({ info }) {
   return (

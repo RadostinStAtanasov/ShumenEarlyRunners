c1bd22080dd2f05429d9c357c4a1e3629a3fbbbc
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-03-24 07:43:57 +0200

    trying to display images

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
   console.error("Error:", err.message);
   res.status(500).send("Internal Server Error");
 });
-// da mahna sekretite
+
 app.use((req, res, next) => {
   const error = new Error("Something went wrong");
   next(error);
--- frontend/src/assets/pages/BlogAndNews/CardBlogAndNews.jsx
+++ frontend/src/assets/pages/BlogAndNews/CardBlogAndNews.jsx
@@ -9,7 +9,11 @@ export default function CardBlogAndNewsPage({ info }) {
       <div className={classes.mainCardContainer}>
         <div className={classes.imageContainer}>
           <a href={`/Блог енд Нюз/${info.id}`}>
-            <img className={classes.imagesBlogAndNews} src={info.pic} alt="" />
+            <img
+              className={classes.imagesBlogAndNews}
+              src={`${info.pic}`}
+              alt=""
+            />
           </a>
         </div>
         <div className={classes.containerPostedBy}>

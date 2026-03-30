f2a4bbb7f20257ed4ca19cf069e4c4f5f8d8da63
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-03-30 07:45:47 +0300

    test public folder if they created from here

--- frontend/src/assets/pages/BlogAndNews/News/MadaraTrail.jsx
+++ frontend/src/assets/pages/BlogAndNews/News/MadaraTrail.jsx
@@ -1,6 +1,6 @@
 import classes from "./MadaraTrail.module.css";
 import Button from "@mui/material/Button";
-import madaraPlato from "../../../images/blogAndNewsImages/madaraRunNewLogo.jpg";
+import madaraPlato from "../../../images/blogAndNewsImages/madataRunNewLogo.jpg";
 
 export default function MadaraTrailPage() {
   return (

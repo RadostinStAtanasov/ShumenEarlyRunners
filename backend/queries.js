2d1e0876b520bd03672af6e3c3f31cf55c36e178
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-04-21 19:56:33 +0300

    Update queries.js

--- backend/queries.js
+++ backend/queries.js
@@ -89,42 +89,42 @@ const createUser = async (req, res) => {
   const { email, password } = req.body;
   let errors = {};
 
-  // if (!isValidEmail(email)) {
-  //   errors.email = "Invalid email.";
-  // } else {
-  //   let emailExist = pool.query(
-  //     "SELECT * FROM users WHERE email = $1",
-  //     [email],
-  //     (error, results) => {
-  //       if (emailExist) {
-  //         errors.email = "Email exists already";
-  //       }
-  //     },
-  //   );
+  if (!isValidEmail(email)) {
+    errors.email = "Invalid email.";
+  } else {
+    let emailExist = pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email],
+      (error, results) => {
+        if (emailExist) {
+          errors.email = "Email exists already";
+        }
+      },
+    );
 
-  //   if (emailExist == mail) {
-  //     errors.email("email already exist from if check");
-  //   }
-  // }
+    if (emailExist == mail) {
+      errors.email("email already exist from if check");
+    }
+  }
 
-  // if (!isValidText(database.password, 6)) {
-  //   errors.password = "Invalid password. Must be at least 6 characters long.";
-  // }
+  if (!isValidText(database.password, 6)) {
+    errors.password = "Invalid password. Must be at least 6 characters long.";
+  }
 
-  // if (Object.keys(errors).length > 0) {
-  //   return res
-  //     .status(422)
-  //     .json({ message: "User signup failed due to validation errors", errors });
-  // }
+  if (Object.keys(errors).length > 0) {
+    return res
+      .status(422)
+      .json({ message: "User signup failed due to validation errors", errors });
+  }
 
-  // try {
-  //   const authToken = createJSONToken(email);
-  //   res
-  //     .status(201)
-  //     .json({ message: "User created.", user: email, token: authToken });
-  // } catch (error) {
-  //   throw error("Token fail");
-  // }
+  try {
+    const authToken = createJSONToken(email);
+    res
+      .status(201)
+      .json({ message: "User created.", user: email, token: authToken });
+  } catch (error) {
+    throw error("Token fail");
+  }
 
   try {
     const hashedPw = await hash(password, 12);
@@ -139,7 +139,7 @@ const createUser = async (req, res) => {
         }
         res
           .status(201)
-          //.json({ message: "User created", user: email, token: authToken })
+          .json({ message: "User created", user: email, token: authToken })
           .send(`User added with ID:`);
       },
     );

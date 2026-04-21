787d72a661fe480184b03309e7186e4aa1a2d954
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-04-21 08:13:25 +0300

    Update queries.js

--- backend/queries.js
+++ backend/queries.js
@@ -127,7 +127,7 @@ const createUser = async (req, res) => {
   // }
 
   try {
-    const hashedPw = hash(password, 12);
+    const hashedPw = await hash(password, 12);
     //const authToken = createJSONToken(email);
 
     pool.query(

fc25de81443db7040c00bd3f9ff0b25377d9fb5d
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-06-05 20:34:05 +0300

    Update queries.js

--- backend/queries.js
+++ backend/queries.js
@@ -104,7 +104,11 @@ const getResults = async (req, res) => {
     const result = await prisma.results.findMany();
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({
+      message: error.message,
+      code: error.code,
+      meta: error.meta,
+    });
   }
 };
 

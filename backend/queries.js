e4772feffa6d8a7dbfd604888b8597ce47438c19
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-06-07 15:34:43 +0300

    return to latest prisma, oldest broke api

--- backend/queries.js
+++ backend/queries.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 //console.log(Object.keys(prisma));
 

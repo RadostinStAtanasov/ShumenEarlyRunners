44b537d1ae7ad7fc73c1d56d3faa3ef2b71532a3
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-06-14 15:39:23 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const router = require("./routers/router");
+//const router = require("./routers/router");
 const app = express();
 const db = require("./queries");
 

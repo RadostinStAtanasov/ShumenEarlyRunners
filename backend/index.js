426444ea984b6e4ea483e373f607604ee00d86f1
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2026-07-05 14:45:33 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/", (req, res) => {
   res.send("WORKING ✅");
 });
